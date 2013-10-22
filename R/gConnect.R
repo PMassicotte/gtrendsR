be087a89ce0ce31517f49e074be2022ccc80151e
Philippe Massicotte <pmassicotte@hotmail.com>
2013-10-22 15:17:41 -0400

    Fixed a bug with quota limits.

--- R/gConnect.R
+++ R/gConnect.R
@@ -1,42 +1,41 @@
-gConnect <-
-function(usr, psw)
+gConnect = function(usr, psw)
 {
-  loginURL <- "https://accounts.google.com/accounts/ServiceLogin"
+  loginURL <- "https://accounts.google.com/ServiceLoginBoxAuth"
   authenticateURL <- "https://accounts.google.com/accounts/ServiceLoginAuth"
   
   require(RCurl)
   
   ch <- getCurlHandle()
   
-#   curlSetOpt(curl = ch,
-#              ssl.verifypeer = FALSE,
-#              useragent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13",
-#              timeout = 60,
-#              followlocation = TRUE,
-#              cookiejar = "./cookies",
-#              cookiefile = "./cookies")
-  
   curlSetOpt(curl = ch,
              ssl.verifypeer = FALSE,
              useragent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13",
              timeout = 60,
+             httpheader = list(continue = "http://www.google.com/trends"),
              followlocation = TRUE,
              cookiejar = "./cookies",
              cookiefile = "./cookies")
   
-  ## do Google Account login
-  loginPage <- getURL(loginURL, curl = ch)
   
-  require(stringr)
+  ## Perform Google Account login
+  loginPage <- getURL(loginURL, curl=ch)
+  
   galx.match <- str_extract(string = loginPage,
                             pattern = ignore.case('name="GALX"\\s*value="([^"]+)"'))
+  
+
   galx <- str_replace(string = galx.match,
                       pattern = ignore.case('name="GALX"\\s*value="([^"]+)"'),
                       replacement = "\\1")
   
-  authenticatePage <- postForm(authenticateURL, .params = list(Email = usr, Passwd = psw, GALX = galx), curl = ch, .opts = list(verbose = F))
+  authenticatePage <- postForm(authenticateURL, .params=list(Email=usr, Passwd=psw, GALX = galx, PersistentCookie= "yes", continue = "http://www.google.com/trends"), curl=ch)
   
   
+  if(getCurlInfo(ch)$response.code == 200) {
+    print("Google login successful.")
+  } else {
+    print("Google login not successful. Check your login credentials!")
+  }
   
   return(ch)
 }
--- R/gTrends.R
+++ R/gTrends.R
@@ -1,12 +1,11 @@
-gTrends <-
-function(ch, geo = 'all', query)  
+gTrends = function(ch, geo = 'all', query)  
 {
   
   ## get Google Insights results CSV
   trendsURL <- "www.google.com/trends/trendsReport?"
   resultsText <- getForm(trendsURL, .params = list(q = query, geo = geo, export = 1, hl = 'en'), curl = ch, .opts = list(verbose = F))
   
-  print(resultsText)
+  #print(resultsText)
   
   ## Sometimes we reach quota limit, in that case stop!
   if(any(grep("QUOTA", resultsText))){
