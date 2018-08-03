3dceff277763a443d524d223498efe65d1476209
VictorYammouni <34738798+VictorYammouni@users.noreply.github.com>
2018-08-03 11:21:38 +0200

    setHandleParameters.R
    Documentation added

--- R/setHandleParameters.R
+++ R/setHandleParameters.R
@@ -1,3 +1,29 @@
+#' If \code{gtrends} should be used behind a proxy,
+#' especially with NTLM authentication mode, 
+#' you need to set the proxy parameters and credentials using "setHandleParameters" function
+#'		
+#' @param user A string specifying your username
+#' @param password A string specifying your password
+#' @param domain A string specifying the authentication domain 
+#' @param proxyhost A string specifying the Proxy host DNS or IP address
+#' @param proxyport A numeric specifying the Proxy Port : 8080 (default)
+#' @param proxyauth A numeric specifying the Proxy Authentication Method : 
+#'   0 for NONE
+#'   1 for BASIC
+#'   2 for DIGEST
+#'   4 for NEGOTIATE
+#'   8 for NTLM
+#'  15 for ANY (default)
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(gtrendsR)
+#'
+#' setHandleParameters(user="xxxx",password="*******",domain="mydomain",proxyhost = "10.111.124.113")
+#' res <- gtrends(c("nhl", "nba"), geo = c("CA", "US"))
+#' }
+#' @export
 setHandleParameters<-function(
 		user=NULL,
 		password=NULL,
