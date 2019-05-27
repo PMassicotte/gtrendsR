738e9f69440fa9885b3d647e4c51b61f3ae238e6
josiahparry <josiah.parry@gmail.com>
2019-05-27 18:13:42 -0400

    refactor interest_over_time function to be able to parse dates better

--- R/zzz.R
+++ R/zzz.R
@@ -110,7 +110,7 @@ get_widget <- function(comparison_item, category, gprop, hl, cookie_url, tz) {
   widget <- myjs$widgets
 }
 
-interest_over_time <- function(widget, comparison_item,tz) {
+interest_over_time <- function(widget, comparison_item, tz) {
   payload2 <- list()
   # if there is a mix of search and topic terms requests are all shifted by one
   # for some reason. Maybe there is a better fix for this. I don't understand
@@ -195,89 +195,22 @@ interest_over_time <- function(widget, comparison_item,tz) {
      (length(unique(widget$request$comparisonItem[[2]]$time))==1)|
      (is.null(widget$request$comparisonItem[[2]]) )
      ){
-    n <- nrow(df) # used to reshape the data
+    df <- as_tibble(df)
     
-    df <- reshape(
-      df,
-      varying = names(df)[2:ncol(df)],
-      v.names = "hits",
-      direction = "long",
-      timevar = "temp",
-      times = names(df)[2:ncol(df)]
-    )
+    names(df) <- c("trend_date",  str_extract(df_names, keyword))
     
-    df$temp <- NULL
-    
-    df <- cbind(
-      df,
-      comparison_item[rep(seq_len(nrow(comparison_item)), each = n), 1:3],
-      row.names = NULL
-    )
-    
-    df$geo <- ifelse(df$geo == "", "world", df$geo)
-    df$gprop <- ifelse(widget$request$requestOptions$property[1] == "", "web", widget$request$requestOptions$property[1])
-    df$category <- widget$request$requestOptions$category[1]
-    names(df)[1] <- "date"
-    df$id <- NULL
-    
-    # Format the returned date
-    if(all(grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2}$",df$date))){
-      df$date <- as.POSIXct(df$date,format="%Y-%m-%d",tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)),asUTC=T)
-    }else if(all(grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}$",df$date))){
-      df$date <- as.POSIXct(df$date,format="%Y-%m-%dT%H",tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)),asUTC=T)
-    }else{
-      df$date <- gsub("^([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}).*$","\\1",df$date)
-      df$date <- as.POSIXct(df$date,format="%Y-%m-%dT%H:%M:%S",tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)),asUTC=T)
-    }
-
-  }else{
-    n <- nrow(df) # used to reshape the data
-    kw <- payload2$comparisonItem$complexKeywordsRestriction[[1]][[1]]$value
-    kw <- gsub("[[:blank:]-]",".",kw)
-    dates <- df[,which(!grepl(kw,names(df)))]
-    
-    if(all(sapply(lapply(dates,function(x) grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2}$",x)),function(y) all(y)))){
-      dates <- data.frame(lapply(dates,
-                                 function(x) as.POSIXct(x,
-                                                format="%Y-%m-%d",
-                                                tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)))))
-    }else if(all(sapply(lapply(dates,function(x) grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}$",x)),function(y) all(y)))){
-      dates <- data.frame(lapply(dates,
-                                 function(x) as.POSIXct(x,
-                                                        format="%Y-%m-%dT%H",
-                                                        tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)))))
-    }else{
-      dates <- data.frame(lapply(dates,
-                                 function(x)
-                                   gsub("^([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}).*$","\\1",x)))
-      dates <- data.frame(lapply(dates,
-                                 function(x) as.POSIXct(x,
-                                              format="%Y-%m-%dT%H:%M:%S",
-                                              tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)))))
-    }
-    
-    # dates <- data.frame(lapply(dates,function(x) anytime::anytime(x,tz=paste0("GMT",ifelse(tz>=0,"+","-"),(abs(tz)/60)),asUTC=T)))
-    
-    hits <- df[,which(grepl(kw,names(df)))]
-    
-    for(jj in 1:NCOL(dates)){
-      df_tmp <- data.frame(dates[jj],hits[jj])
-      df_tmp2 <- comparison_item[rep(jj,n), 1:3]
-      
-      df_tmp2[,1] <- ifelse(df_tmp2[,1] == "", "world", df_tmp2[,1])
-      df_tmp2[,3] <- ifelse(widget$request$requestOptions$property[1] == "", "web", widget$request$requestOptions$property[1])
-      df_tmp2[,4] <- widget$request$requestOptions$category[1]
-      if(jj==1){
-        df_res <- cbind(df_tmp,df_tmp2)
-        names(df_res) <- c("date","hits","geo","time","gprop","category")
-      }else{
-        df_tmp3 <- cbind(df_tmp,df_tmp2)
-        names(df_tmp3) <- c("date","hits","geo","time","gprop","category")
-        df_res <- rbind(df_res,df_tmp3)
-      }
-    }
-    df <- df_res
+    df <-df %>% 
+      gather(keyword, hits, -trend_date) %>% 
+      rename(date = trend_date) %>% 
+      mutate(gprop = ifelse(widget$request$requestOptions$property[1] == "", 
+                          "web",
+                          widget$request$requestOptions$property[1]),
+             category = widget$request$requestOptions$category[1],
+             geo = ifelse(comparison_item[[1,1]] == "", "world", comparison_item[[1,1]]),
+             date = as.Date(ifelse(nchar(date) == 7, paste0(date, "-01"), date))
+             )
   }
+    
 
   return(df)
 }
@@ -436,10 +369,6 @@ map_tz2min <- function(timezone){
   round((unclass(as.POSIXct(format(Sys.time(),"%Y-%m-%d %H:%M:%S",tz="UTC")))-unclass(as.POSIXct(format(Sys.time(),"%Y-%m-%d %H:%M:%S",tz=timezone))))/60)
 }
 
-map_min2tz <- function(min){
-    
-}
-
-
-
-
+#map_min2tz <- function(min){
+#    
+#}
