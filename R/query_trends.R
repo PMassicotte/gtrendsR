070ccf6783e45506c4d980f7fde55a5efd6d2a74
josiahparry <josiah.parry@gmail.com>
2019-05-27 13:25:11 -0400

    update print method. Move print and plot method to query_trends.R

--- R/query_trends.R
+++ R/query_trends.R
@@ -4,7 +4,7 @@
 #'
 #' @param search_terms A character vector containing the search terms of interest
 #' @param from The beginning date of the query
-#' @param to The beginning date of the query
+#' @param to The end date of the query
 #' @param ... arguments passed to \code{gtrends()}. See ?gtrends for more information including geography, language, and time-zone.
 #'
 #' @export
@@ -30,9 +30,11 @@ query_trends <- function(search_terms, from = NA, to = NA, ...) {
   
   
   if (length(search_terms) <= 5) {
+    message(paste("Querying", length(search_terms), "search terms."))
     searched_trends <- gtrends(search_terms, time = time, ...) %>%
       list()
   } else {
+    message(paste("Querying", length(search_terms), "search terms individually."))
     searched_trends <- search_terms %>%
       map(gtrends, time = time, ...)
   }
@@ -45,3 +47,64 @@ query_trends <- function(search_terms, from = NA, to = NA, ...) {
   )
   
 }
+
+
+#' Plot Google Trends interest over time
+#'
+#' @param x A \code{\link{gtrends}} object.
+#' @param ... Additional parameters passed on in method dispatch. Currently not
+#'   used.
+#'
+#' @import ggplot2
+#'
+#' @return A ggplot2 object is returned silently.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' res <- gtrends("nhl", geo = c("CA", "US"))
+#' plot(res)
+#' }
+plot.gtrends <- function(x, ...) {
+  df <- get_interest(x)
+  
+  
+  df$legend <- paste(df$keyword, " (", df$geo, ")", sep = "")
+  
+  p <- ggplot(df, aes_string(x = "date", y = "hits", color = "legend")) +
+    geom_line() +
+    xlab("Date") +
+    ylab("Search hits") +
+    ggtitle("Interest over time") +
+    theme_bw() +
+    theme(legend.title = element_blank())
+  
+  print(p)
+  invisible(p)
+}
+
+
+#'  Print gtrends object
+#' 
+#' @param x gtrends object
+#' @importFrom dplyr pull group_by summarise
+#' @export
+print.gtrends <- function(x, ...) {
+  cat((crayon::bold("~ Google Trends Results ~\n")))
+  cat("\nSearch Terms: ")
+  cat(paste(get_interest(x) %>%
+              dplyr::pull(keyword) %>%
+              unique(),
+            sep = " "), sep = ", ")
+  cat("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~ summary ~~~~~~~~~~~~~~~~~~~~~~~~\n")
+  print(x %>%
+          get_interest() %>%
+          group_by(keyword) %>%
+          summarise(max_hits = max(hits),
+                    min_hits = min(hits),
+                    from = as.Date(min(date)),
+                    to = as.Date(max(date))),
+        width = 0)
+  invisible(x)
+}
+
