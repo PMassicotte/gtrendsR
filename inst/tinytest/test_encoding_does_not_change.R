ad2593479ad104d4e9c4c154abdf5b4527898308
Philippe Massicotte <pmassicotte@hotmail.com>
2020-05-17 09:19:30 -0400

    Skip encoding tests on Windows.

--- inst/tinytest/test_encoding_does_not_change.R
+++ inst/tinytest/test_encoding_does_not_change.R
@@ -1,4 +1,11 @@
+
+# Encoding UTF-8 not well supported on Windows, skip tests
+if ( Sys.info()[['sysname']] == "Windows"){
+  exit_file("Cannot test this on Windows")
+}
+
 # Japanese
 kw <- "赤"
 res <- gtrends(kw)
 expect_identical(unique(res$interest_over_time$keyword), kw)
+
