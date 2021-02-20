0c24a07f4b8b72f6e5e02b43138f02984360cc89
Philippe Massicotte <pmassicotte@hotmail.com>
2021-02-20 14:08:18 -0500

    Forgot to remove this.

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,9 +1,4 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
-
-  # Check if we have access to internet. If not, do not perform the test. Note
-  # that curl::nslookup() is cross-platform and should work on all OS.
-  if (curl::has_internet()) {
-    tinytest::test_package("gtrendsR")
-  } 
+  tinytest::test_package("gtrendsR")
 }
